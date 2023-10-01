35dc266201548b3a45f3f524eb723755e06f4458
Joe Reid <joe@joereid.com>
2023-09-30 22:24:58 -0400

    flag stuff

--- main.go
+++ main.go
@@ -16,10 +16,9 @@ import (
 var (
 	serversFile = ".q2servers.config" // default name, should be in home directory
 	config      = flag.String("config", "", "Specify a server data file")
-	//property    = flag.String("property", "", "Output only this specific server var")
-	format = flag.String("format", "", "What should we output")
-	name   = flag.String("name", "", "Regex pattern for the name to lookup")
-	group  = flag.String("group", "", "Regex pattern for the group to lookoup")
+	format      = flag.String("format", "", "What should we output")
+	name        = flag.String("name", "", "Regex pattern for the name to lookup")
+	group       = flag.String("group", "", "Regex pattern for the group to lookoup")
 )
 
 func main() {
